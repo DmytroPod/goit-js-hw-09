f896b5799f934b0d9d9df2ccdc80f92a80f5b258
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-29 12:52:12 +0000

    Deploying to gh-pages from @ DmytroPod/goit-js-hw-09@7025a1312aca4f50eafc5d8036a35fa462cf4a5f 🚀

---
+++ 01-color-switcher.22c3d62c.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")},a=0;t.start.addEventListener("click",(function(){t.start.disabled=!0,t.stop.disabled=!1,a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stop.addEventListener("click",(function(){clearInterval(a),t.start.disabled=!1,t.stop.disabled=!0})),t.stop.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.22c3d62c.js.map
