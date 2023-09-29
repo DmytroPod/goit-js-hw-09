7025a1312aca4f50eafc5d8036a35fa462cf4a5f
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-29 15:51:44 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btn = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let id = 0;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btn.start.addEventListener('click', startHandle);
+btn.stop.addEventListener('click', stopHandle);
+btn.stop.disabled = true;
+
+function startHandle() {
+  btn.start.disabled = true;
+  btn.stop.disabled = false;
+
+  id = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopHandle() {
+  clearInterval(id);
+
+  btn.start.disabled = false;
+  btn.stop.disabled = true;
+}
